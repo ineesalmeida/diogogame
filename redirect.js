5cf40ac9014dc2aafa8c69ec58aaa55d229fb595
Ines Almeida <ines.almeida@perspectum.com>
2020-04-19 15:24:04 +0100

    prevent players from going back (not working for chrome

--- redirect.js
+++ redirect.js
@@ -1,3 +1,9 @@
+
+window.location.hash="no-back-button";
+window.location.hash="Again-No-back-button";//again because google chrome don't insert first hash into history
+window.onhashchange=function(){window.location.hash="no-back-button";}
+
 setTimeout(function(){
     window.location.href = 'index.html';
-}, 2000);
+}, 3000);
+
